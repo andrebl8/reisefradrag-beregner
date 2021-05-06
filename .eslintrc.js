ef68a79dc1aa7109f3f47dc5e553aec760faaa0f
André <andre.b.lindeloff@gmail.com>
2021-05-06 20:14:46 +0200

    config of eslint and prittier

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript-prettier',
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    'no-console': 0,
+    'react/prop-types': 0,
+    'react/destructuring-assignment': 0,
+    'react/static-property-placement': 0,
+    'jsx-a11y/alt-text': 0,
+    'react/jsx-props-no-spreading': 0,
+    'import/order': [
+      1,
+      {
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        groups: [
+          'builtin',
+          'unknown',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index'
+        ],
+        'newlines-between': 'always'
+      }
+    ],
+  }
+};
