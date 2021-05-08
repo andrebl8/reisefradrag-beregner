d4f1c027d1f5b76b305564a840e2bd0b7857460a
André <andre.b.lindeloff@gmail.com>
2021-05-08 16:43:01 +0200

    Tweaked fetch to take a request body as param

--- src/api/reisefradrag.ts
+++ src/api/reisefradrag.ts
@@ -1,18 +1,23 @@
+import { IFormValues } from '../interfaces/IFormValues';
+
 const url = '';
-const options = {
-  method: 'POST',
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json;charset=UTF-8'
-  },
-  body: JSON.stringify({
-    arbeidsreiser: [
-      { km: 91, antall: 180 },
-      { km: 378, antall: 4 }
-    ],
-    besoeksreiser: [{ km: 580, antall: 4 }],
-    utgifterBomFergeEtc: 4850
-  })
+const createOptions = (body: IFormValues) => {
+  return {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json;charset=UTF-8'
+    },
+    body: JSON.stringify(body)
+  };
 };
 
-export { options, url };
+const calculateTaxReduction = (postBody: IFormValues) => {
+  const options = createOptions(postBody);
+
+  fetch(url, options).then((response) => {
+    console.log(response);
+  });
+};
+
+export default calculateTaxReduction;
